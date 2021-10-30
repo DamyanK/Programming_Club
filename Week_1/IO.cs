90ca2b4f49a6a7921f53f6cfb508e55e797b8273
Damyan Kushev <53000149+DamyanK@users.noreply.github.com>
2021-10-30 14:16:38 +0300

    Initial

---
+++ Week_1/IO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Console.ReadKey();
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("How old are you? : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, {name}. You are {age} y.o.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week_1/Math_Operations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // | + | - | * | / | % |  ---- Аритметични оператори
+            // 
+            // > | < | >= | <= | == | !=
+            //
+            // = --- int a = 5
+
+
+            Console.Write("Sample for Math.Round -> ");
+            Console.WriteLine(Math.Round(7.9639812381, 2));
+            Console.Write("Sample for Math.Ceiling -> ");
+            Console.WriteLine(Math.Ceiling(7.9639812381));
+            Console.Write("Sample for Math.Floor -> ");
+            Console.WriteLine(Math.Floor(7.9639812381));
+            Console.Write("Sample for Math.Sqrt -> ");
+            Console.WriteLine(Math.Sqrt(9));
+            Console.Write("Sample for Math.Pow -> ");
+            Console.WriteLine(Math.Pow(9, 2));
+            Console.Write("Sample for Math.Abs -> ");
+            Console.WriteLine(Math.Abs(-17));
+            Console.Write("Sample for Math.Max -> ");
+            Console.WriteLine(Math.Max(5, 10));
+            Console.Write("Sample for Math.Min -> ");
+            Console.WriteLine(Math.Min(5, 10));
+            Console.ReadKey();
+        }
+    }
+}
