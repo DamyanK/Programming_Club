d0ca299a7aad2fb4e71419923e9fd8f2034f241e
Damyan Kushev <53000149+DamyanK@users.noreply.github.com>
2021-11-27 11:45:37 +0200

    week 5

---
+++ Week_5/Base_excs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // |5;50| -> n*n -/> try again
+            while (n < 5 || n > 50)
+            {
+                Console.Write("Try again: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(n * n);
+
+            // Въвеждат се цели числа и се изписват, докато не се въведе 0
+
+            int a = int.Parse(Console.ReadLine());
+            int min = a;
+            int max = a;
+            int sum = 0;
+            while (a != 0)
+            {
+                if (a > max)
+                {
+                    max = a;
+                }
+                if (a < min)
+                {
+                    min = a;
+                }
+                sum = sum + a;
+                a = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Max = {max} \nMin = {min} \nSum = {sum}");
+
+            //int sum = 0;
+            //for (int i = 11; i <= 42; i++)
+            //{
+            //    sum = sum + i;
+            //    Console.WriteLine($"[{i}] = {sum}");
+            //}
+            //Console.WriteLine("Total = " + sum);
+
+            int mul = 1;
+            for (int i = 5; i <= 25; i = i + 5)
+            {
+                mul = mul * i;
+                Console.WriteLine($"[{i}] = {mul}");
+            }
+            Console.WriteLine("Total = " + mul);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week_5/Loops.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Броячен цикъл (итеративен)
+            for (int i = 0; i < 10; i++) // i++ <=> i = i + 1 <=> i += 1
+            {
+                Console.WriteLine(i + 1);
+            }
+            Console.WriteLine("Цикълът приключи изпълнението на задачата.");
+
+            // Индуктивни цикли
+
+            // - цикъл с предусловие
+            double a = 1.6;
+            while (a < 3)
+            {
+                Console.WriteLine(a);
+                a = a + 0.17;
+                // Има възможност да не се изпълни нито веднъж.
+            }
+            Console.WriteLine("Цикълът приключи изпълнението на задачата.");
+
+            // - цикъл със следусловие
+            double b;
+            do
+            {
+                Console.Write("Enter value for b: ");
+                b = double.Parse(Console.ReadLine());
+                // Задължително се изпълнява поне един път.
+            } while (b < 0.0 || b > 14.3); // |0;14.3|
+            Console.WriteLine("Цикълът приключи изпълнението на задачата.");
+
+            Console.ReadKey();
+        }
+    }
+}
