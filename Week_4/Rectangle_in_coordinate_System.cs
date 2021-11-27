8399e51fe1bd351d94452b374f416a4159fa806e
Damyan Kushev <53000149+DamyanK@users.noreply.github.com>
2021-11-27 09:59:50 +0200

    week 4

---
+++ Week_4/Rectangle_in_coordinate_System.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 2;
+            int y1 = 2;
+            int x2 = 6;
+            int y2 = 5;
+
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+
+            if ((y == y1 || y == y2) && (x1 < x && x < x2))
+            {
+                Console.WriteLine("Border");
+            }
+            else if ((x == x1 || x == x2) && (y1 < y && y < y2))
+            {
+                Console.WriteLine("Border");
+            }
+            else if ((x1 < x && x < x2) && (y1 < y && y < y2))
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Outside");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week_4/Tour_Price.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour_Price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            int proverka = 0;
+            if (n >= 1 && n <= 20)
+            {
+                proverka = 1;
+            }
+            else
+            {
+                if (n <= 100)
+                {
+                    proverka = 2;
+                }
+                else
+                {
+                    if (n <= 5000)
+                    {
+                        proverka = 3;
+                    }
+                }
+            }
+
+            switch (proverka)
+            {
+                case 1:
+                    {
+                        Console.Write("D/N? > ");
+                        string dn = Console.ReadLine();
+                        switch (dn.ToLower())
+                        {
+                            case "d": Console.WriteLine(0.7 + n * 0.79 + "лв."); break;
+                            case "n": Console.WriteLine(0.7 + n * 0.9 + "лв."); break;
+                            default: Console.WriteLine("greshka"); break;
+                        }
+                    } break;
+                case 2: Console.WriteLine(n * 0.09 + "лв."); break;
+                case 3: Console.WriteLine(n * 0.06 + "лв."); break;
+            }
+
+            //if (n <= 5000 && n >= 1)
+            //{
+            //    if (n <= 20)
+            //    {
+            //        Console.Write("day/night -> ");
+            //        string dayCycle = Console.ReadLine();
+
+            //        switch (dayCycle.ToLower())
+            //        {
+            //            case "day": Console.WriteLine(0.7 + n * 0.79 + "лв."); break;
+            //            case "night": Console.WriteLine(0.7 + n * 0.9 + "лв."); break;
+            //            default: Console.WriteLine("Invalid choice! Try again with day or night!"); break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (n <= 100)
+            //        {
+            //            Console.WriteLine(n * 0.09 + "лв.");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(n * 0.06 + "лв.");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Try again!");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
